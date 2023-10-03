ffad1b63f733356a2092dde3851e045f39db5490
MohamedYassin-J <jammelimohamedyassin@gmail.com>
2023-10-03 17:21:46 +0100

    Upload Greetings.Nuget.Demo to GitHub

---
+++ Greetings.cs
@@ -0,0 +1,16 @@
+﻿namespace Greetings.Nuget.Demo;
+
+public class Greetings
+{
+    public void SayHello() => Console.WriteLine("Hello From Greetings.Nuget.Demo Package");
+
+    public void SayHi() => Console.WriteLine("Hi From Greetings.Nuget.Demo Package");
+
+    public void SayGoodMorning() => Console.WriteLine("Good Morning From Greetings.Nuget.Demo Package");
+
+    public void SayGoodNight() => Console.WriteLine("Good Night From Greetings.Nuget.Demo Package");
+
+    public void SayGoodAfternoon() => Console.WriteLine("Good Afternoon From Greetings.Nuget.Demo Package");
+
+    public void SayGreetings() => Console.WriteLine("Greetings From Greetings.Nuget.Demo Package");
+}
